f531079609e59487246d04c8606621731781108d
kimthan <kimthan85@gmail.com>
2018-05-11 08:11:44 +0200

    updated qragain

--- src/components/GoToCashier.js
+++ src/components/GoToCashier.js
@@ -6,11 +6,11 @@ class GoToCashier extends Component {
         let ordernr = this.props.ordernr.size.length+this.props.ordernr.base.length+this.props.ordernr.dish.length
         
         return (
-            <div style={{textAlign:'center', marginTop:-20}}>
+            <div style={{textAlign:'center', marginTop:-10}}>
                 <h2   >Ange:</h2>
-                <h2   style={{fontSize:80, marginBottom:100, marginTop:-20}}>#{ordernr}</h2>
+                <h2   style={{fontSize:80, marginBottom:100, marginTop:-10}}>#{ordernr}</h2>
                 
-                
+                <h2   >QR KOD:</h2>
                 <img style={{marginTop:-20}} src="/bilder/qrcode.png" alt="qrcode" />
                 </div>
            
